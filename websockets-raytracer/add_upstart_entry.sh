9f7814ee6b7839d185c02d2cffa5b1a1d4e5a3cc
Ubuntu <ubuntu@ip-10-162-119-111.us-west-1.compute.internal>
2010-09-30 04:40:00 +0000

    Fixed an error with upstart script

--- websockets-raytracer/add_upstart_entry.sh
+++ websockets-raytracer/add_upstart_entry.sh
@@ -3,7 +3,7 @@ if [ ! "`whoami`" = "root" ]; then
   exit
 fi
 
-echo -e "start on startup \n\
+echo "start on startup \n\
 exec python `pwd`/server.py \n\
 respawn \n\
 respawn limit 10 90" > /etc/init/raytracer.conf
